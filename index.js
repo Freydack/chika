1d98f47a0556899202e3bd65b00cf511d068a1c6
Tsuzue <32021643+Freydack@users.noreply.github.com>
2019-03-09 20:18:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const bot = new Discord.Client()
 var prefix = "*"
 
 setInterval(() => {
-    const nombre = 6;
+    const nombre = 2;
     const random = Math.floor(Math.random() * (nombre - 1) + 1);
     switch(random) {
-    case 1: bot.user.setActivity('*commandes', {type: "WATCHING"}); break;
-    case 2: bot.user.setActivity('taunt Monika', {type:"PLAYING"}); break;
-    case 3: bot.user.setActivity("des jeux de sociétés", {type:"PLAYING"}); break;
+    case 1: bot.user.setActivity('*commands', {type: "WATCHING"}); break;
+    case 2: bot.user.setActivity('with Tsuzue#9563', {type:"PLAYING"}); break;
     }
 }, 10000);
 bot.on("guildCreate", guild => {
@@ -31,14 +30,14 @@ bot.on('message', function (message) {
     }
     const journal = message.guild.channels.find(channel => channel.name === "chika-logs");
     if (message.author.bot || !message.guild) return;
-    if (message.content.startsWith(prefix + 'pdp')) {
+    if (message.content.startsWith(prefix + 'pc')) {
         
         let user = message.mentions.users.first();
         let Uav = user.displayAvatarURL;
         message.channel.send(Uav);
-    } else if(message.content.startsWith(prefix + "lien")) {
+    } else if(message.content.startsWith(prefix + "link")) {
             message.channel.createInvite()
-            .then(invite => message.channel.send(`Voici le lien : https://discordapp.com/invite/${invite.code}`))
+            .then(invite => message.channel.send(`Here's the link : https://discordapp.com/invite/${invite.code}`))
     }else if (message.content.toLowerCase().includes('aqua')) {
         message.channel.send("<a:aquadance:553253696360677377>")
 } else if (message.content.toUpperCase().startsWith('MUDA')) {
@@ -50,26 +49,25 @@ bot.on('message', function (message) {
     const mentions = message.mentions.users.first()
     if(message.guild.me.hasPermission("MANAGE_ROLES")) {
         if(arole == null) {
-            message.channel.send("Aucun role trouvé avec ce nom");
+            message.channel.send("No role found with this name.");
             return;
         };
             message.guild.member(mentions).addRole(arole);
-            message.channel.send(`Le role **${arole}** a bien été ajouté a **${mentions.username}!**`);
-        }   else message.channel.send("Tu n'as pas la permission de faire ça")
+            message.channel.send(`The role **${arole}** has been added to **${mentions.username}!**`);
+        }   else message.channel.send("You can't do that :x")
         }else if (message.content.toLowerCase().startsWith('boi')) {
         message.channel.send({files:['https://cdn.discordapp.com/attachments/533018309524979712/550040716626231300/image0.jpg']});
     }else if(message.content.startsWith(prefix + "mute")) {
         const mention = message.mentions.users.first();
         const arole = message.guild.roles.find(r => r.name === "mute");
         if(message.guild.me.hasPermission("MANAGE_ROLES")) {
-            guild.channels.array().forEach(channel => channel.overwritePermissions(guild.roles.find(r => r.name === "mute"), {"SEND_MESSAGES": false}))
                 if(mention == null) {
-                message.channel.send("Aucune personne trouvée ");
+                message.channel.send("Nobody with that name was found :x");
                 return;
                 }
             }
             message.guild.member(mention).addRole(arole);
-            message.channel.send("Membre bien mute !")
+            message.channel.send("Member muted !")
         }else if (message.content.toLowerCase().includes('kawaii')) {
             message.channel.send({files:['https://media1.tenor.com/images/3cee627ab9f455a0f14739ba5edbf81a/tenor.gif']});
         }else if (message.content.toLowerCase().includes('euuu')) {
@@ -91,19 +89,15 @@ bot.on('message', function (message) {
     message.delete();
     user.send(message.content.slice(26));
     message.author.send("message bien envoyé ! message :" + message.content.slice(26))
-} else  if (message.content.toLowerCase().includes('hehe boi')) {
-    message.channel.send( {files: ["./random/heheboi.gif"]})
-} else if (message.content === 'Marie sur Brawlhalla') {
-    message.channel.send("La fameuse ...", {files:["https://media.giphy.com/media/12DLBuhDtQCBIk/giphy.gif"]})
-} else  if (message.content.toUpperCase() === 'C PO NICE') {
-    message.channel.send("c'est toi qui est po nice !")    
+} else  if (message.content.toLowerCase().includes('hentai')) {
+    message.channel.send( {files: ["https://i.ytimg.com/vi/7FSw8a8k198/maxresdefault.jpg"]})
 } else if(message.content.startsWith(prefix + "cookie")) {
     var number = 5;
     var imageNumber = Math.floor (Math.random() * (number -1 + 1)) + 1;
     const mention = message.mentions.users.first();
-    message.channel.send( `:cookie: ${message.author.username} a donné un cookie pour toi, ${mention} ! :cookie: `, {files: ["./cookies/" + imageNumber + ".png"]})
-} else if(message.content.toUpperCase().includes("NUL")) { 
-    message.channel.send("NUL NUL NUL NUL NUL", {files:['https://thumbs.gfycat.com/FarJitteryAustraliansilkyterrier-max-1mb.gif']})
+    message.channel.send( `:cookie: ${message.author.username} gave a cookie for you, ${mention} ! :cookie: `, {files: ["./cookies/" + imageNumber + ".png"]})
+} else if(message.content.includes("BAD")) { 
+    message.channel.send("bad bad bad bad", {files:['https://orig00.deviantart.net/85fe/f/2019/056/7/3/kaguya_sama_love_is_war___chika_fujiwara_2_by_whiteshadow_24-dd0p01g.gif']})
 } else if(message.content.startsWith (prefix + "ban")) {
     const mention = message.mentions.users.first();
     message.delete();
@@ -111,8 +105,8 @@ bot.on('message', function (message) {
     if (mention == null) return;
     if (message.guild.member(mention).hasPermission("BAN_MEMBERS")) return;
     let reason = message.content.slice(prefix.length + mention.toString().length + 6);
-    message.channel.send (mention.username + " a été ban :shrug: ");
-    mention.sendMessage ("t'as été ban de mon serveur parce que " + reason + ":shrug:").then  (d_msg => {
+    message.channel.send (mention.username + " was banned :shrug: ");
+    mention.sendMessage ("You were kicked because : " + reason).then  (d_msg => {
         message.guild.member(mention).ban(reason);
         
     })
@@ -123,8 +117,8 @@ bot.on('message', function (message) {
     if (mention == null) return;
     if (message.guild.member(mention).hasPermission("BAN_MEMBERS")) return;
     let reason = message.content.slice(prefix.length + mention.toString().length + 6);
-    message.channel.send (mention.username + " a été kick :shrug:");
-    mention.sendMessage ("t'as été kick de mon serveur parce que " + reason + ":shrug:").then  (d_msg => {
+    message.channel.send (mention.username + " was kicked :shrug:");
+    mention.sendMessage ("You were kick because : " + reason).then  (d_msg => {
         message.guild.member(mention).kick(reason);
     })
 }else if(message.content.toLowerCase().startsWith(prefix + "calc")) {
@@ -137,11 +131,11 @@ bot.on('message', function (message) {
         message.channel.send(`${content} = **${calculation}**`);
     } catch(error) {console.log(`il y a eu une erreur : ${error}`)}
 } else if(message.content.startsWith(prefix + "support")) {
-    message.channel.send("envoyé en MP !")
-    message.author.send("Si tu as des questions a posé sur moi , va dans ce serveur ou tu pourras trouver mon créateur et lui demander directement https://discord.gg/5Q9gxeG")
+    message.channel.send("Send in private !")
+    message.author.send("If you have some questions or just want to help my creator , go here <:emote:553973306177486863> https://discord.gg/5Q9gxeG")
 
 }else if(message.content.startsWith(prefix + "invite")) {
-message.channel.send("https://discordapp.com/oauth2/authorize?client_id=548540080097067012&scope=bot&permissions=8")
+message.channel.send("https://discordapp.com/oauth2/authorize?client_id=548629235728646192&scope=bot&permissions=8")
 
 }else if(message.content.startsWith(prefix + "random")) {
 var max = message.content.slice(8);
@@ -163,32 +157,32 @@ message.channel.send (embed);
 }else if(message.content.startsWith(prefix + "serveur")) {
     let sicon = message.guild.iconURL;
     let serverembed = new Discord.RichEmbed()
-    .setDescription("information sur le serveur")
+    .setDescription("guild infortmations")
     .setColor('#5C6B69')
     .setThumbnail(sicon)
-    .addField("Le serveur s'appelle ", message.guild.name)
-    .addField("Crée le ", message.guild.createdAt)
-    .addField("Tu as rejoint le ", message.member.joinedAt )
-    .addField("Il y a ", message.guild.memberCount + (" membre(s)"))
+    .addField("The guild is named ", message.guild.name)
+    .addField("Created the ", message.guild.createdAt)
+    .addField("You joined ", message.member.joinedAt )
+    .addField("There are ", message.guild.memberCount + (" member(s)"))
     message.channel.send(serverembed);
-    }else if(message.content.startsWith(prefix + "qui est")) {
+    }else if(message.content.startsWith(prefix + "who is")) {
         const mention = message.mentions.users.first();
         var nom = message.content.slice(9);
         let Uav = mention.displayAvatarURL;
         let embed = new Discord.RichEmbed()
-        .setDescription(`information sur ${nom}`)
+        .setDescription(`who is ${nom}`)
         .setColor('#5C6B69')
         .setThumbnail(Uav)
-        .addField("Le compte s'appelle ", mention.username)
-        .addField("Compte crée le ", mention.createdAt)
-        .addField("Il a rejoint le ",message.guild.member(mention).joinedAt, true)
+        .addField("The account is named ", mention.username)
+        .addField("Created the ", mention.createdAt)
+        .addField("He joins the guild the ",message.guild.member(mention).joinedAt, true)
         message.channel.send(embed);
 
 } else if(message.content.startsWith(prefix + "slap")) {
     var number = 20;
     var imageNumber = Math.floor (Math.random() * (number -1 + 1)) + 1;
     const mention = message.mentions.users.first();
-    message.channel.send( `${mention}, tu te fais taper par ${message.author.username} ! `, {files: ["./slap/" + imageNumber + ".gif"]});
+    message.channel.send( `${mention}, you got hit by ${message.author.username} ! `, {files: ["./slap/" + imageNumber + ".gif"]});
 } else if(message.content.startsWith(prefix + "loli")) {
     var number = 20;
     var imageNumber = Math.floor (Math.random() * (number -1 + 1)) + 1;
@@ -197,18 +191,18 @@ message.channel.send (embed);
 }else if (message.content.startsWith(prefix + "admin")) {
 var embed = new Discord.RichEmbed()
 .setColor("#5C6B69")
-.setTitle("commandes admins")
-.addField("Version Alpha", "Je n'ai pas beaucoup de commandes admins mais j'en aurais d'avantages dans le futur <:emote:553973306177486863> !")
-.addField("*ban @user", "Permet de ban un membre du discord")
-.addField("*kick @user", "Permet de kick un membre du discord")
-.addField("Bienvenue et aurevoir", "quand quelqu'un rentre ou part du serveur, un message sera envoyé dans le salon que je crée!")
-.addField("*clear <montant a supprimer>", "pour supprimer un montant de message définit")
-.addField("*addrole @user <nom du role>", "pour ajouter un role a un utilisateur")
-message.channel.send("Envoyées en mp !")
+.setTitle("admins commands")
+.addField("Alpha version", " I don't have a lot of admins commands but some will be added soon <:emote:553973306177486863> !")
+.addField("*ban @user", "Ban someone")
+.addField("*kick @user", "Kick someone")
+.addField("Hello and Sayonara", "When someone join or leave the guild , I annonce it in my channel with a beautiful gif <:emote:553973306177486863> !")
+.addField("*clear <montant a supprimer>", "to clear chats")
+.addField("*addrole @user <nom du role>", "To add a role to someone")
+message.channel.send("Send in private !")
 return message.author.send(embed);
 }else if(!journal) {
     message.guild.createChannel("chika-logs");
-    message.channel.send("Vous n'aviez pas de channel Chika-logs alors j'en ai crée un <:lewd:553983393742848041> ");
+    message.channel.send("You didn't have a channel named chika-logs so I added one <:lewd:553983393742848041> ");
 }else if(message.content.startsWith(prefix + "nazi")) {
     let gay = Math.round(Math.random() * 100);
     const mention = message.mentions.users.first();
@@ -216,7 +210,7 @@ return message.author.send(embed);
         return
         }
     let gayembed = new Discord.RichEmbed()
-        .setTitle(`**Je pense que ${mention.username} est ${gay}% Nazi!**`);
+        .setTitle(`**I think ${mention.username} is ${gay}% Nazi!**`);
     message.channel.send(gayembed);
     }else if(message.content.startsWith(prefix + "love")) {
         let love = Math.round(Math.random() * 100);
@@ -225,33 +219,32 @@ return message.author.send(embed);
             return
         }
         let gayembed = new Discord.RichEmbed()
-            .setTitle(`**:heartpulse:${message.author.username} et ${mention.username} sont a ${love}% d'amour!:heartpulse:**`);
+            .setTitle(`**:heartpulse:${message.author.username} and ${mention.username} are ${love}% in love!:heartpulse:**`);
         message.channel.send(gayembed);
-}else if(message.content.startsWith(prefix + "commandes")) {
+}else if(message.content.startsWith(prefix + "commands")) {
     var embed = new Discord.RichEmbed()
-    .setTitle("Je suis en version Alpha")
+    .setTitle("I'm an Alpha version")
     .setColor("934C4D")
-    .setDescription("Mon créateur vient juste de me commencer donc il reste encore pleins de choses a faire que j'apprendrais dans le futur :smile:")
-    .addField("Commandes", "Voici une liste de mes commandes")
-    .setFooter("Mon prefix est '*'")
-    .addField("serveur", "Donne des informations sur le seveur")
-    .addField("invite", "Pour avoir mon lien d'invitation pour m'ajouter sur pleins de beaux serveurs :smile:")
-    .addField("cookie @user", "Donne un cookie a la personne désignée parce que c'est toujours bien de donner des cookies")    
-    .addField("calc" , "Je peux calculer tout ce que vous voulez temps que ça reste linéaire :smile:")
-    .addField("slap @user", "Pour frapper les méchants")
-    .addField("*pdp @user", "pour admirer sa magnifique photo de profil ou celle de quelqu'un d'autre")
-    .addField("Les commandes cachées", "il y a pleins d'autres commandes cachées qui ne necessitent pas le prefix :smile: a toi de les trouver !Un exemple :Sayori")
+    .addField("Commands", "Here's a list of my commands <:emote:553973306177486863>")
+    .setFooter("My prefix is '*'")
+    .addField("guild", "Give you some informations about the guild")
+    .addField("invite", "To get my link to add me in a lot of lovely guild")
+    .addField("cookie @user", "Give a cookie to someone because it's good !")    
+    .addField("calc" , "To calculate <:emote:553973306177486863>")
+    .addField("slap @user", "To slap the bad persons")
+    .addField("pc @user", "To show a profile picture because it is very beautiful (or not)")
+    .addField("Les commandes cachées", "There's a lot of hidden commands ! An example :Sayori")
     .setThumbnail("https://66.media.tumblr.com/0a27a139ead026cb9d9166087ae0ecb9/tumblr_pla7g897Bt1xlkja9o2_250.gif")
-    .addField("*support", "Pour contacter mon créateur si tu as des questions a propos de moi")
-    .addField("*loli", "Commande pour faire apparaitre pleins de Lolis ... attention la police pourrait arriver...")
-    .addField("*hug", "Pour faire des câlins aux gens/bots(les bots aussi ont le droit d'avoir des câlins!) Que tu aimes!")
-    message.channel.send("envoyées en mp !, il existe des commandes pour administrateur , pour les avoir : `*admin`")
+    .addField("*support", "To contact my creator if you have some questions or you just want to chat with him because he is nice <:emote:553973306177486863>")
+    .addField("*loli", "To spawn picture of loli ... wait .. The police can come")
+    .addField("*hug", "To hug the peoples you like!")
+    message.channel.send("Send in private !, There are also admin commands, to get them : `*admin`")
     message.author.send(embed);
     } else if(message.content.includes(prefix + "hug")) {
         var number = 30;
         var imageNumber = Math.floor (Math.random() * (number -1 + 1)) + 1;
         const mention = message.mentions.users.first();
-        message.channel.send( `${message.author.username} te fait un calin , ${mention}. Kawaiiiii ! `, {files: ["./hug/" + imageNumber + ".gif"]});
+        message.channel.send( `${message.author.username} is hugging you , ${mention}. Kawaiiiii ! `, {files: ["./hug/" + imageNumber + ".gif"]});
     }else if(message.content.toLowerCase().includes("je suis noir")) {
             message.channel.send({files:["https://cdn.discordapp.com/attachments/548247199679512576/549309722734362634/image0.jpg"]})
     }else if(message.content.startsWith(prefix + "clear")) {
@@ -259,14 +252,14 @@ return message.author.send(embed);
                 var amount = message.content.slice(6);
                 if (!message.member.hasPermission("MANAGE_MESSAGES")) return;
                         message.channel.bulkDelete(amount);
-                        journal.send(`${message.author.username} vient de supprimer ${amount} messages`)
+                        journal.send(`${message.author.username} just deleted ${amount} messages`)
                 
-}else if(message.content.startsWith(prefix + "annonce")) {
+}else if(message.content.startsWith(prefix + "announce")) {
         const journal = message.guild.channels.find(channel => channel.name === "chika-logs");
             var annonce = message.content.slice(8);
             if(!message.member.hasPermission("ADMINISTRATOR")) return;
             var embed = new Discord.RichEmbed()
-            .setTitle(`nouvelle annonce de ${message.author.username} !`)
+            .setTitle(`New announce from ${message.author.username} !`)
             .setDescription(`${annonce}`)
             .setColor("934C4D")
             message.delete()
@@ -282,7 +275,7 @@ return message.author.send(embed);
 bot.on('guildMemberAdd', member => {
 const journal = member.guild.channels.find(channel => channel.name === "chika-logs");
 var embed = new Discord.RichEmbed()
-.setDescription(`${member.displayName} vient d'arriver dans le serveur , bienvenue <:emote:553973306177486863>`)
+.setDescription(`${member.displayName} join the guild , hello <:emote:553973306177486863>`)
 .setColor("#5C6B69")
 .setImage("https://i.redd.it/y07i2h02dog21.gif");
 journal.send(embed);
@@ -292,11 +285,11 @@ bot.on("guildMemberRemove", member => {
 const journal = member.guild.channels.find(channel => channel.name === "chika-logs");
 if(journal) {
 var embed = new Discord.RichEmbed()
-.setDescription(`${member.displayName} vient de quitter le serveur `)
+.setDescription(`${member.displayName} left the guild`)
 .setColor("#5C6B69")
 .setImage("https://i.imgur.com/LxG1qGl.gif");
 journal.send(embed);
 }
 });
 
-bot.login('NTQ4NjI5MjM1NzI4NjQ2MTky.D2Vs2g.AbvqiFXsIOlsnkFageJmBvx9nGM');
+bot.login('process.env.TOKEN');
